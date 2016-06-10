3f4065bc770e806c11a58c03f2240d719e9ee0cb
raimund <rm@km-it.de>
2016-06-10 13:50:56 +0200

    Attempted to merge https://github.com/rm-km-it/MySQLConverterTool/commit/6336fb18056e9f377039e241a2b3a5213eee0f8d

--- Function/RealEscapeString.php
+++ Function/RealEscapeString.php
@@ -27,15 +27,11 @@ class MySQLConverterTool_Function_RealEscapeString extends MySQLConverterTool_Fu
         if (count($params) == 1) {
             list($string) = $this->extractParamValues($params);
             $conn = $this->ston_name;
-            $warning = 'If no global connection has been opened already the converter cannot use mysqli_real_escape_string() to emulate the function. We add a runtime test if a connection exists, if not trigger_error() gets used to throw an E_USER_ERROR. It would be a security bug to do anything else. You must fix our code manually!';
-            $ret = sprintf('((isset(%s) && is_object(%s)) ? %s(%s, %s) : ((trigger_error("[MySQLConverterToo] Fix the mysql_escape_string() call! This code does not work.", E_USER_ERROR)) ? "" : ""))',
-                    $conn,
-                    $conn,
-                    $this->new_name,
-                    $conn,
-                    $string
-                );
-        } elseif (count($params) == 2) {
+            $warning = null;
+            $ret = sprintf('%s(%s, %s)', $this->new_name, $conn, $string);              
+            
+        } else if (count($params) == 2) {
+
             list($string, $conn) = @$this->extractParamValues($params);
             $warning = null;
             $ret = sprintf('%s(%s, %s)', $this->new_name, $conn, $string);
@@ -46,9 +42,9 @@ class MySQLConverterTool_Function_RealEscapeString extends MySQLConverterTool_Fu
 
         return array($warning, $ret);
     }
-
-    public function getConversionHint()
-    {
-        return 'Emulated using mysqli_real_escape_string and the default connection if no connection is given. If no default connection exists an E_USER_ERROR gets thrown.';
+    
+    function getConversionHint() {
+        
+        return 'Emulated using mysqli_real_escape_string and the default connection if no connection is given. A default connection has to be made before.';
     }
 }
